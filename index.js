dbca04ae8ec363d1ae76f32daed4a125bdde4a41
Aurimas Mickys <aurimasmickys@gmail.com>
2021-02-11 17:08:27 +0200

    Feat/calculate snap points (#2)
    * calculate snap points inside scrollview wrapper
    
    * tvos scroll fix
    
    * recursive childrens map
    
    * ability to define snap points from outside of comp
    
    * fix snap points adding to array
    
    * fix: sometimes child is undefined or null when && syntax is used in jsx to render a component
    
    * refactor: fixing typo, removing left over ';' symbol
    
    * fix childrens itteration based on key
    
    * handle on layout from outer
    
    * add scrollTo method
    
    Co-authored-by: Ričardas Noreika <36963057+RicardasN@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,17 +1,84 @@
-import React from 'react';
-import { ScrollView, Platform } from "react-native";
+import React, { useState, useImperativeHandle, useRef } from 'react';
+import { ScrollView, Platform } from 'react-native';
 import AndroidTVSnapScrollView from './index.androidtv';
 import TVOSSnapScrollView from './index.tvos';
 
-const SV = React.forwardRef(({ children, ...props }, ref) => {
+let snapPointsHolder = {};
+
+const SV = React.forwardRef(({ children, snapPoints: propsSnapPoints, ...props }, ref) => {
+    const [snapPoints, setSnapPoints] = useState([]);
+    const scrollViewRef = useRef(null);
+
+    const onLayout = (event, index) => {
+        const { layout } = event.nativeEvent;
+
+        snapPointsHolder = { ...snapPointsHolder, [index]: Math.round(layout.y) };
+        setSnapPoints(Object.values(snapPointsHolder).map(point => parseInt(point, 10)));
+    };
+
+    useImperativeHandle(ref, () => ({
+        onLayout: (event, index) => {
+            onLayout(event, index);
+        },
+        scrollTo: args => {
+            scrollViewRef.current.scrollTo(args);
+        },
+    }));
+
+    const recursiveMap = ch =>
+        React.Children.map(ch, child => {
+            if (!React.isValidElement(child)) {
+                return child;
+            }
+
+            if (child.props.children) {
+                child = React.cloneElement(child, {
+                    children: recursiveMap(child.props.children),
+                });
+            }
+
+            if (child.props.setSnapPoint) {
+                return React.cloneElement(child, {
+                    ...child.props,
+                    onLayout: event => onLayout(event, child.key),
+                });
+            }
+
+            return child;
+        });
+
+    const content = React.Children.map(children, child =>
+        child ? React.cloneElement(recursiveMap(child)[0]) : null
+    );
     if (Platform.isTVOS) {
-        return <TVOSSnapScrollView ref={ref} {...props}>{children}</TVOSSnapScrollView>;
-    }
-    if (Platform.OS === 'android' && Platform.isTV) {
-        return <AndroidTVSnapScrollView ref={ref} {...props}>{children}</AndroidTVSnapScrollView>;
+        return (
+            <TVOSSnapScrollView
+                ref={scrollViewRef}
+                {...props}
+                snapPoints={propsSnapPoints || snapPoints}
+            >
+                {content}
+            </TVOSSnapScrollView>
+        );
     }
 
-    return <ScrollView ref={ref} {...props}>{children}</ScrollView>;
+    if (Platform.OS === 'android' && Platform.isTV) {
+        return (
+            <AndroidTVSnapScrollView
+                ref={scrollViewRef}
+                {...props}
+                snapPoints={propsSnapPoints || snapPoints}
+            >
+                {content}
+            </AndroidTVSnapScrollView>
+        );
+    }
+
+    return (
+        <ScrollView ref={scrollViewRef} {...props}>
+            {children}
+        </ScrollView>
+    );
 });
 
 export default SV;
